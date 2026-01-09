1aef81ca8e6d0e6ad078634d3c87772148e4fbb8
samuelcherry <samuelcherry92@gmail.com>
2026-01-09 11:31:11 -0700

    added auto scrolling

--- client/src/components/ChatArea.tsx
+++ client/src/components/ChatArea.tsx
@@ -1,18 +1,23 @@
 
-import {useState, useEffect, useContext} from 'react';
+import {useState, useEffect,useRef, useContext} from 'react';
 import {useMessageContext} from '../utils/MessageContext'
 
 
 function ChatArea() {
 
-//create an array in local storage of messages so that messages can be appended to the array on sending
-//useContext to hold the messages and move them across components
-	
+	const bottomRef = useRef<HTMLDivElement | null>(null);
 	const {messages, setMessages} = useMessageContext();
 	const {username, setUsername} = useMessageContext();
-	return (
+	
+
+	useEffect(() => {
+		bottomRef.current?.scrollIntoView({ behavior: "smooth"});
+	}, [messages]);
+
+
+return (
 		<>
-			<div className="bg-gray-100 h-9/10 outline-solid outline-2 outline-gray-400 rounded-t-lg">
+			<div className="bg-gray-100 h-9/10 outline-solid outline-2 outline-gray-400 rounded-t-lg overflow-y-auto">
 				{messages.map( message => (
 					<div key={message.id} className="flex justify-end">
 						<h1 className= "p-6 rounded-xl bg-blue-400 m-3 w-1/2 color-white">
@@ -21,7 +26,8 @@ function ChatArea() {
 							{message.content}
 						</h1>
 					</div>
-				))} 
+				))}
+				<div ref={bottomRef}/>
 			</div>
 		</>
 	)
