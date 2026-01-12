9a7fc839bf52f9fcf293406e3e2ce3f65e6663be
samuelcherry <samuelcherry92@gmail.com>
2026-01-12 15:16:43 -0700

    fixed typo in env

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { 
 		cors:{
-			origin: process.emv.FRONTEND_URL,
+			origin: process.env.FRONTEND_URL,
 			methods: ["GET", "POST"],
 		},
         });
