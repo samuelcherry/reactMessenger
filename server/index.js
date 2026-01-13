8fa7b68e2076baa8b83e7211eb20d9dc39b591f0
samuelcherry <samuelcherry92@gmail.com>
2026-01-13 10:11:00 -0700

    changed cors permissions

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { 
 		cors:{
-			origin: process.env.FRONTEND_URL | "http://localhost:5173",
+			origin: process.env.FRONTEND_URL,
 			methods: ["GET", "POST"],
 			credentials: true,
 			},
